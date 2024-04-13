55e0ba1e373af0d03c0ea3365f4be507bf6d64be
Juan Florez <juanmanuelflorezescobar@gmail.com>
2024-04-12 22:52:58 -0500

    Ajuste de la vista del listado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -18,8 +18,12 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
-        return view ('comuna.index',['comunas' => $comunas]);
+    // $comunas = Comuna::all();
+    $comunas = DB::table('tb_comuna')
+    ->join('tb_municipio' , 'tb_comuna.muni_codi', '=' , 'tb_municipio.muni_codi')
+    ->select('tb_comuna.*' , 'tb_municipio.muni_nomb')
+    ->get();
+    return view('comuna.index' , ['comunas' => $comunas]);
     }
 
     /**
