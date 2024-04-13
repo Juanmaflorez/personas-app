3c925046719c61f92acb6b5524ade324579cba87
Juan Florez <juanmanuelflorezescobar@gmail.com>
2024-04-13 00:08:02 -0500

    departamento

---
+++ app/Http/Controllers/DepartamentoController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Departamento;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DepartamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $departamentos = DB::table('tb_departamento')
+            ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+            ->select('tb_departamento.*', 'tb_pais.pais_nomb')
+            ->get();
+    
+        return view('departamento.index', ['departamentos' => $departamentos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $departamentos = DB::table('tb_departamento')
+            ->orderBy('depa_nomb')
+            ->get();
+        
+        return view('departamento.new', ['departamentos' => $departamentos]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $departamentos = DB::table('tb_departamento')
+         ->get();
+    
+        return view('departamento.index', ['departamentos' => $departamentos]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $departamento = Departamento::find($id);
+        
+        return view('departamento.edit', ['departamento' => $departamento]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $departamento = Departamento::find($id);
+        $departamento->update($request->all());
+    
+        return redirect()->route('departamentos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $departamento = Departamento::find($id);
+        $departamento->delete();
+    
+        $departamentos = DB::table('tb_departamento')
+          ->get();
+    
+        return view('departamento.index', ['departamentos' => $departamentos]);
+    }
+}
